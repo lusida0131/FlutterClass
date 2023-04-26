ba52f6aebe94e9b9b032e1bc9e8725fc8e081a88
lusida0131 <ks12b9189@naver.com>
2023-04-26 22:00:34 +0900

    Flutter day2

---
+++ codingsf/myapp/lib/main1.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); // compiler에게 시작 함수 알려주기 위해
+
+class MyApp extends StatelessWidget {
+  // Myapp 커스텀 위젯 생성
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // MaterialApp 위젯으로 설정하기
+      title: 'First App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(), //
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [Text('data'), Text('data'), Text('data')],
+        ),
+      ),
+    );
+  }
+}
---
+++ codingsf/myapp/lib/main2.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); // compiler에게 시작 함수 알려주기 위해
+
+class MyApp extends StatelessWidget {
+  // Myapp 커스텀 위젯 생성
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // MaterialApp 위젯으로 설정하기
+      title: 'Character Card',
+      home: MyCard(), //
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BANDO'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('data'),
+            Text('data'),
+            Text('data'),
+          ],
+        ),
+      ),
+    );
+  }
+}
